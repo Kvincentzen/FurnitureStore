9579c0f5d13bfc4e2c9dc8aebd364c419b84fe2d
Kristian KV. Vincentzen <krvi@pc-tec.local>
2021-06-09 14:34:23 +0200

    Home show products

---
+++ FurnitureFront/src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private ProductUrl = 'api/Products/';
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.ProductUrl)
+  }
+}
