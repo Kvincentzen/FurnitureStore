11b615931154eb7f8813ee3c94593216d8011a9a
PC-TEC\krvi <Kvincentzen@gmail.com>
2021-08-11 13:40:18 +0200

    blah

--- FurnitureFront/src/app/product-detail/product-detail.component.ts
+++ FurnitureFront/src/app/product-detail/product-detail.component.ts
@@ -7,7 +7,7 @@ import { Product } from '../models/product';
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
-})
+}) 
 export class ProductDetailComponent implements OnInit {
 
   product: Product;
